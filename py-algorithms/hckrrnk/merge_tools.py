89295356cbfc316bef2fec1bcc7600e019de31f4
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-08 17:08:48 +0200

    merge tools

---
+++ py-algorithms/hckrrnk/merge_tools.py
@@ -0,0 +1,15 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    temp = []
+    len_temp = 0
+    for item in string:
+        len_temp += 1
+        if item not in temp:
+            temp.append(item)
+        if len_temp == k:
+            print (''.join(temp))
+            temp = []
+            len_temp = 0
+
+if __name__ == '__main__':
+    merge_the_tools('AABCAAADA', 3)
