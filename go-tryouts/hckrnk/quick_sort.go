72d08b4bc240ecae6fedef444e9b5cf03be48eec
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-16 17:08:05 +0200

    quick sort

---
+++ go-tryouts/hckrnk/quick_sort.go
@@ -0,0 +1,31 @@
+package hckrnk
+
+func QuickSort(n int, arr []int) []int {
+	pivot := arr[0]
+	e := make([]int, 0)
+	l := make([]int, 0)
+	r := make([]int, 0)
+	helpers := make([][]int, 0)
+	res := make([]int, 0)
+
+	for i, _ := range arr {
+		if arr[i] < pivot {
+			l = append(l, arr[i])
+		}
+		if arr[i] > pivot {
+			r = append(r, arr[i])
+		}
+		if arr[i] == pivot {
+			e = append(e, arr[i])
+		}
+	}
+	helpers = append(helpers, e, l, r)
+	for _, v := range helpers {
+		// fmt.Println(v)
+		res = append(res, v...)
+
+	}
+	// fmt.Println(res)
+
+	return res
+}
