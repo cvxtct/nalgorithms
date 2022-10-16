4e226ff0df5d7c1a0d0adc569382b280a942558c
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-16 17:07:37 +0200

    basics

---
+++ go-tryouts/basics/basics.go
@@ -0,0 +1,16 @@
+package basics
+
+import "fmt"
+
+func Basics() {
+	fmt.Println("foo bar")
+
+	var whatToSay string
+	var i int
+
+	whatToSay = "foo bar"
+	fmt.Println(whatToSay)
+
+	i = 2
+	fmt.Println("I is set to", i)
+}
