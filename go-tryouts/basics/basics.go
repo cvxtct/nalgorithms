d1ee8dc83d90aa90b1f9fc65182504b3d5dc1265
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-17 17:13:39 +0200

    decision

--- go-tryouts/basics/basics.go
+++ go-tryouts/basics/basics.go
@@ -62,11 +62,44 @@ func changeUsingPointer(s *string) {
 func Decisions() {
 	var isTrue bool
 
-	isTrue = true
+	isTrue = false
 
 	if isTrue {
 		log.Println("isTrue is, ", isTrue)
 	} else {
 		log.Println("isTrue is", isTrue)
 	}
+
+	cat := "cat"
+	if cat == "cat" {
+		log.Println("Cat is ", cat)
+	} else {
+		log.Println("Cat is not cat.")
+	}
+
+	myNum := 100
+	// use only one level of this
+	if myNum > 99 && !isTrue {
+		log.Println("myNum is greater than 99 and isTrue is set to true")
+	} else if myNum < 100 && isTrue {
+		log.Println("foo")
+	} else if myNum == 100 || isTrue {
+		log.Println("bar")
+	} else if myNum > 1000 && !isTrue {
+		log.Println("baz")
+	}
+
+	myVar := "Cat"
+	// use this if you have more conditions
+	switch myVar {
+	case "Cat":
+		log.Println("Cat is set to cat.")
+	case "Dog":
+		log.Println("Cat is set to dog.")
+	case "Dino":
+		log.Println("Cat is set to dino.")
+	default:
+		log.Println("Nothing to set")
+	}
+
 }
