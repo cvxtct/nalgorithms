987b698d3f15d9574e233a6fba3501037cfe0c77
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-18 19:13:36 +0200

    counting sort

---
+++ go-tryouts/hckrnk/counting_sort.go
@@ -0,0 +1,28 @@
+package hckrnk
+
+func CountingSort(n int, arr []int) []int {
+	count := make([]int, 100)
+	seen := make(map[int]bool)
+	res := []int{}
+
+	for i, _ := range arr {
+		if !seen[arr[i]] {
+			seen[arr[i]] = true
+		}
+		if seen[arr[i]] {
+			count[arr[i]]++
+		}
+	}
+
+	for i, _ := range arr {
+		if count[i] > 0 {
+			cnt := count[i]
+			for cnt > 0 {
+				res = append(res, i)
+				cnt--
+			}
+		}
+	}
+
+	return res
+}
