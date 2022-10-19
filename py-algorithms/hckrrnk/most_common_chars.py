11290a40a5ed3e9ee72bff6fef80baa013257463
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-19 13:58:44 +0200

    clean up

--- py-algorithms/hckrrnk/most_common_chars.py
+++ py-algorithms/hckrrnk/most_common_chars.py
@@ -1,15 +1,10 @@
-import re
-import operator
-
 from collections import Counter
 def most_common_char(strng):
   s = sorted(strng)
-
-
   frequency = Counter(list(s))
   for k, v in frequency.most_common(3):
     print(k, v)
 
 if __name__ == '__main__':
     string = "aabbbccde"
-    print(most_common_char(string))
+    most_common_char(string)
