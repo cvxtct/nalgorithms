891ff661c04f965f61ac17075f26d16109ce3197
Attila, Balázs <ifearnoghosts@gmail.com>
2022-10-30 19:17:21 +0100

    finished counting sort

--- py-algorithms/hckrrnk/counting_sort_full.py
+++ py-algorithms/hckrrnk/counting_sort_full.py
@@ -1,16 +1,22 @@
 def counting_sort(n, arr):
     arr_len = len(arr) # this is actualy given
-    arr_max = max([i[0] for i in arr])
-    arr_min = min([i[0] for i in arr])
+    arr_max = int(max([i[0] for i in arr]))
+    arr_min = int(min([i[0] for i in arr]))
+    
     count_arr_len = arr_max + 1 - arr_min
 
     count = [0] * count_arr_len
     seen = set()
 
     for number in arr:
-        count[number[0]-arr_min] += 1
+        count[int(number[0])-arr_min] += 1
     
     print(count)
+
+    # replace character with '-' in arr[i][1]
+    for i in range(0, arr_len//2):
+        arr[i][1] = '-'
+
 
     for i in range(1, count_arr_len):
         # this identify the slot where the actual elem belongs to
@@ -20,13 +26,40 @@ def counting_sort(n, arr):
 
     ordered = [0] * arr_len
     for i in reversed(range(0, arr_len)):
-        ordered[count[arr[i][0] - arr_min] -1] = arr[i]
-        count[arr[i][0] - arr_min] -= 1
-
-    return ordered
+        ordered[count[int(arr[i][0]) - arr_min] -1] = arr[i]
+        count[int(arr[i][0]) - arr_min] -= 1
+    
+    to_print = []
+    print(" ".join(map(str, [i[1] for i in ordered])))
+    # return ordered
 
 
 if __name__ == '__main__':
     n = 20
     arr = [[0, 'ab'], [6, 'cd'], [0, 'ef'], [6, 'gh'], [4, 'ij'], [0, 'ab'], [6, 'cd'], [0, 'ef'], [6, 'gh'], [0, 'ij'], [4, 'that'], [3, 'be'], [0, 'to'], [1, 'be'], [5, 'question'], [1, 'or'], [2, 'not'], [4, 'is'], [2, 'to'], [4, 'the']]
+    inp = """20
+            0 ab
+            6 cd
+            0 ef
+            6 gh
+            4 ij
+            0 ab
+            6 cd
+            0 ef
+            6 gh
+            0 ij
+            4 that
+            3 be
+            0 to
+            1 be
+            5 question
+            1 or
+            2 not
+            4 is
+            2 to
+            4 the"""
+    # arr2 = []
+    # for _ in range(inp):
+    #     arr.append(input().rstrip().split())
+    # print(arr2)
     print(counting_sort(n, arr))
