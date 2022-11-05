0c31f257cc1174ed6c7fc66daa45933f9ed070e5
Attila, Balázs <ifearnoghosts@gmail.com>
2022-11-05 17:19:28 +0100

    rotate array

---
+++ py-algorithms/misc/rotate_array.py
@@ -0,0 +1,28 @@
+def rotate_array__apr_one(array_, d):
+    # to the left
+    # k = array_.index(d)
+    temp_ = array_[d:]
+    temp_ = temp_ + array_[:d]
+    for a,t in zip(array_, temp_):
+        print(a,t)
+        array_[a-1] = t
+    print(array_)
+
+def rotate_array__apr_two(array_, n):
+    p = 1
+
+    while p >= n:
+        last = array_[0]
+        for i in range(n-1):
+            array_[i] = array_[i+1]
+        array_[n - 1] = last
+        p+=1
+    print(array_)
+
+
+
+if __name__ == '__main__':
+    array_ = [1,2,3,4,5,6,7]
+    d = 3
+    rotate_array__apr_one(array_, d)
+    rotate_array__apr_two(array_, d)
