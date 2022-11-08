a17e8d7906132ea5052763109dc84570decff7c3
Attila, Balázs <ifearnoghosts@gmail.com>
2022-11-08 11:44:09 +0100

    rotate array fix

--- py-algorithms/misc/rotate_array.py
+++ py-algorithms/misc/rotate_array.py
@@ -10,20 +10,19 @@ def rotate_array__apr_one(array_, d):
 
 def rotate_array__apr_two(array_, d, n):
     p = 1
-
     while p <= d:
         last = array_[0]
-        for i in range(n-1):
+        for i in range(0, n-1):
             array_[i] = array_[i+1]
         array_[n - 1] = last
         p+=1
-    print(array_)
-
 
 
 if __name__ == '__main__':
     array_ = [1,2,3,4,5,6,7]
     d = 3
     n = len(array_)
+    print(array_)
     #rotate_array__apr_one(array_, d)
     rotate_array__apr_two(array_, d, n)
+    print(array_)
