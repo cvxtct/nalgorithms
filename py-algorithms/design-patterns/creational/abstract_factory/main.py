e48c5cd4f58c4327f0c8351a8117215927e665ad
Attila, Balázs <ifearnoghosts@gmail.com>
2022-11-08 12:52:33 +0100

    client code try out

--- py-algorithms/design-patterns/creational/abstract_factory/main.py
+++ py-algorithms/design-patterns/creational/abstract_factory/main.py
@@ -127,3 +127,30 @@ class ConcreteProductB2(AbstractProductB):
         """
         result = collaborator.useful_function_a()
         return f"The result of the B2 collaborating with the ({result})"
+
+# CLIENT CODE
+
+def client_code(factory: AbstractFactory) -> None:
+    """
+    The client code works with factories and products only through abstract
+    types: AbstractFactory and AbstractProduct. This lets you pass any factory
+    or product subclass to the client code without breaking it.
+    """
+    product_a = factory.create_product_a()
+    product_b = factory.create_product_b()
+
+    print(f"{product_b.useful_function_b()}")
+    print(f"{product_b.another_useful_function_b(product_a)}", end="")
+
+
+if __name__ == "__main__":
+    """
+    The client code can work with any concrete factory class.
+    """
+    print("Client: Testing client code with the first factory type:")
+    client_code(ConcreteFactory1())
+
+    print("\n")
+
+    print("Client: Testing the same client code with the second factory type:")
+    client_code(ConcreteFactory2())
